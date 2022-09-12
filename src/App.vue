c3f6e286a908d972af066f1fec18985bcda8897e
David <kopeld@gmail.com>
2022-09-12 15:58:59 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import AppHeader from "./components/AppHeader.vue";
+import Products from "./components/Products.vue";
+</script>
+
+<template>
+  <AppHeader />
+  <Products />
+</template>
+
+<style scoped>
+    
+</style>
---
+++ src/components/Products.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import Product from "./Product.vue";
+
+export default defineComponent({
+  name: "Products",
+  components: { Product },
+});
+</script>
+
+<template>
+  <section class="products">
+    <Product />
+    <Product />
+    <Product />
+    <Product />
+    <Product />
+    <Product />
+  </section>
+</template>
+
+<style scoped>
+.products {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+
+</style>
+
